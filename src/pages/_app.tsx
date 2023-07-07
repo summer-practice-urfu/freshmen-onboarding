f81c29b3cdaf99a448fc36503421463680bad0e0
Danil Gribanov <gribanovwork@yandex.ru>
2023-07-07 21:15:11 +0500

    feat: end initialization

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app';
+import GlobalStyle from '@/style/globalStyles';
+import '@/style/fonts.css';
+import ScrollBarProvider from '@/context/ScrollBarContext';
+import 'swiper/css';
+import { SWRConfig } from 'swr';
+import fetcher from '@/lib/fetcher';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    return (
+        <SWRConfig value={{ fetcher }}>
+            <ScrollBarProvider>
+                <GlobalStyle />
+                <Component {...pageProps} />
+            </ScrollBarProvider>
+        </SWRConfig>
+    );
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import { GetStaticProps } from 'next';
+import React from 'react';
+import Layout from '@/components/common/Layout/Layout';
+import { BasePageProps } from '@/interfaces';
+
+interface IndexProps extends BasePageProps {
+    /* Page props*/
+}
+
+const Index: React.FC<IndexProps> = (props) => {
+    return (
+        <Layout meta={props.meta} header={props.header} sandwich={props.sandwich}>
+            <h1 style={{ textAlign: 'center', marginTop: '10rem' }}>
+                Hello, World!
+            </h1>
+
+            {/* Page body */}
+        </Layout>
+    );
+};
+
+export const getStaticProps: GetStaticProps<IndexProps> = async () => {
+    return {
+        props: {
+            meta: {
+                title: 'Title',
+                description: 'description',
+                keywords: 'keywords'
+            },
+            header: {},
+            sandwich: {}
+        },
+        revalidate: 1
+    };
+};
+
+export default Index;
